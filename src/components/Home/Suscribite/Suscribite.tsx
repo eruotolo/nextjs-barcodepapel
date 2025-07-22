871e09364a0480cfce717bc24d08ff96b4085fe2
edgardoruotolo <edgardoruotolo@crowadvance.com>
2025-07-22 12:59:43 -0400

    Task: Cambios CSS | Date: 22-07-2025 | Version: 2.32

--- src/components/Home/Suscribite/Suscribite.tsx
+++ src/components/Home/Suscribite/Suscribite.tsx
@@ -5,7 +5,7 @@ import { subscribeToNewsletter } from '@/actions/Administration/Newsletter/mutat
 
 export default function Suscribite() {
     return (
-        <div className="relative z-50 flex w-full justify-center px-4 py-8 sm:py-12 md:w-[700px] md:py-16">
+        <div className="relative z-50 flex w-full justify-center py-8 sm:py-12 md:w-[700px] md:py-16">
             <div className="w-full max-w-sm sm:max-w-md md:max-w-lg">
                 <iframe
                     src="https://barcodepapel.substack.com/embed"
@@ -13,10 +13,11 @@ export default function Suscribite() {
                     height="320"
                     style={{
                         border: '1px solid #EEE',
-                        background: 'white',
+                        background: 'transparent',
                     }}
                     frameBorder="0"
                     scrolling="no"
+                    className="h-auto w-[350px] md:h-auto md:w-full"
                 ></iframe>
             </div>
         </div>
